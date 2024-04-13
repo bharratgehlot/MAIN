89ceb44c0c048f65bbdae1224e54657f1680db36
bharratgehlot <161546117+bharratgehlot@users.noreply.github.com>
2024-04-13 22:11:27 +0530

    first commit

---
+++ application.py
@@ -0,0 +1,2 @@
+#Oceanofpeople app - A simple crud app that manage data of employees, student, workers along with names, email, number and a photo.
+#Technologies Used in this Project - Flask, sqlite3, 
