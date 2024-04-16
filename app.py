5dbe5ed43ead150dcc513abb77812c4663b2862b
bharratgehlot <161546117+bharratgehlot@users.noreply.github.com>
2024-04-16 12:09:56 +0530

    Minor Changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#Oceanofpeople app - A simple crud app that manage data of employees, student, workers along with names, email, number and a photo.
+#People's Databank app - A simple crud app that manage data of employees, student, workers along with names, email, number and a photo.
 #Technologies Used in this Project - Flask, sqlite3, 
 
 from flask import Flask, render_template, request, redirect, url_for
